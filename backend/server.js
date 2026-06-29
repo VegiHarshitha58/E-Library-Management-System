e1c37cc413c103c22184167b721836eba273889f
VegiHarshitha58 <vegiharshitha58@gmail.com>
2026-06-29 14:36:04 +0530

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
@@ -17,11 +18,12 @@ app.use(
     express.static("avatars")
 );
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Harshitha@1435",
-    database: "elibrary"
-});  
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+}); 
 db.connect((err) => {
 
     if(err){
